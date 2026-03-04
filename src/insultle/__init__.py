8396c0d3c6f33157e9ac4531d4a6dc1e3d0d0953
Dafne <dafne.belardinelli@gmail.com>
2026-03-04 12:47:59 +0100

    nuova funzione
    schermata inziziale

--- src/insultle/__init__.py
+++ src/insultle/__init__.py
@@ -13,6 +13,58 @@ def main() -> None:
     schermo = pygame.display.set_mode((Larghezza_Schermo, Altezza_Schermo)) 
     pygame.display.set_caption("Insultle") 
 
+    FontLettere = pygame.font.SysFont('Impact', 60)
+
+    imgSfondo = pygame.image.load("sfondo-verde-chiaro.webp") 
+    imgSfondo = pygame.transform.scale(imgSfondo,(Larghezza_Schermo,Altezza_Schermo))
+
+    tasti_mouse = {
+        "GIOCA": pygame.Rect(67,510, 200,70),
+        "CLASSIFICA": pygame.Rect(300,510, 300,70),
+    }
+
+    running = True
+    while running:
+
+        schermo.blit(imgSfondo, (0, 0))
+
+        # Disegno pulsanti
+        for tasto, rect in tasti_mouse.items():
+            pygame.draw.rect(schermo, "white", rect)
+            testo = FontLettere.render(tasto, True, "black")
+            schermo.blit(testo, (rect.x + 10, rect.y + 5))
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos_mouse = pygame.mouse.get_pos()
+
+                for tasto, rect in tasti_mouse.items():
+                    if rect.collidepoint(pos_mouse):
+                        if tasto == "GIOCA":
+                            gioco()
+                        elif tasto == "CLASSIFICA":
+                            print("CLASSIFICA")
+
+        pygame.display.flip()
+
+    pygame.quit()
+    
+    
+def gioco() -> None:
+    
+    Larghezza_Schermo = 822
+    Altezza_Schermo = 745
+    schermo = pygame.display.set_mode((Larghezza_Schermo, Altezza_Schermo)) 
+    pygame.display.set_caption("Insultle") 
+
     imgSfondo = pygame.image.load("sfondoINSULTLE.jpg") 
     imgSfondo = pygame.transform.scale(imgSfondo,(Larghezza_Schermo,Altezza_Schermo))
 
@@ -248,4 +300,4 @@ def main() -> None:
 #------------------------------------
     
 if __name__ == "__main__":
-    print(main())
+    main()
